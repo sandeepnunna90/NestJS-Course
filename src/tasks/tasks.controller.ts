28f100159983ace2835d88d0ae25ff0109732c25
Sandeep Nunna <snunna@centrak.com>
2023-01-16 23:07:56 -0500

    Creating a Task (Part 2 - Controller)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +10,27 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // NestJs provides 2 ways to retrieve request parameters
+  // using the body decorator below
+  // downside to this is the body can have any type of properties
+  // we will need to add some validations inside to restrict
+  //   @Post()
+  //   createTask(@Body() body) {
+  //     console.log('body', body);
+  //   }
+
+  // Second way to use individual properties and
+  // decorate them with body decorator
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log('title', title);
+    console.log('description', description);
+
+    return this.tasksService.createTask(title, description);
   }
 }
