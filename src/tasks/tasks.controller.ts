3164bc1c1d574a7a4ffdfad94374a2a569f46892
Sandeep Nunna <snunna@centrak.com>
2023-01-22 13:18:36 -0500

    Validation: Update Task Status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status-dto';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -55,8 +56,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() UpdateTaskStatusDto: UpdateTaskStatusDto,
   ): Task {
+    const { status } = UpdateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
