9d350c7e07a0d22574b2b60aebd4b8bcdcf2f91b
Sandeep Nunna <snunna@centrak.com>
2023-01-29 16:22:04 -0500

    Restrict user to fetch TaskById and UpdateStatus to only their Tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -17,8 +17,8 @@ export class TasksService {
   getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
     return this.taskRepository.getTasks(filterDto, user);
   }
-  async getTaskById(id: string): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({ where: { id, user } });
 
     if (!found) {
       throw new NotFoundException(`Task with ID: "${id}" not found`);
@@ -40,8 +40,12 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
 
     task.status = status;
     await this.taskRepository.save(task);
