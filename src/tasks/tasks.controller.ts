68bce87ca6b94b192ebe27d0a9d7322e9f8a2e65
Sandeep Nunna <snunna@centrak.com>
2023-01-29 16:43:01 -0500

    Log user & data in createTasks handler

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -31,6 +31,11 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(
+        createTaskDto,
+      )}`,
+    );
     return this.tasksService.createTask(createTaskDto, user);
   }
 
